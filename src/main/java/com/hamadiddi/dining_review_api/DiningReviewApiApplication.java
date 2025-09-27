b6aa6590881d9c54b5faa14696d13bd21474979d
Hamadi Iddi <hamaditaufik@gmail.com>
2025-09-27 13:12:42 +0300

    Initial commit

---
+++ src/main/java/com/hamadiddi/dining_review_api/DiningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.hamadiddi.dining_review_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApiApplication.class, args);
+	}
+
+}
