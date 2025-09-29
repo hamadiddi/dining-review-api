791110431ff8c092f7f64030529d2285997e80bc
Hamadi Iddi <hamaditaufik@gmail.com>
2025-09-29 09:17:36 +0300

    define user entity

---
+++ src/main/java/com/hamadiddi/dining_review_api/model/User.java
@@ -0,0 +1,33 @@
+package com.hamadiddi.dining_review_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String city;
+    private String state;
+    private String zipcode;
+
+    private Boolean isPeanutAllergic;
+    private Boolean isEggAlergic;
+    private Boolean isDairyAllergic;
+
+}
